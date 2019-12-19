b8c712558ab68541e6072d088dcc9e2885f9af47
barimale <mateusz.wolnica@gmail.com>
2019-12-19 23:20:13 +0100

    .

--- ConfigurationManager.Api/Helper/IAdapterFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using ConfigurationManager.Api.Helper.Adapters;
-
-namespace ConfigurationManager.Api.Helper
-{
-    public interface IAdapterFactory
-    {
-        T GetAdapter<T>(IReadOnly manager) where T : BaseAdapter;
-    }
-}
